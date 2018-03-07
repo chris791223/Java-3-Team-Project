705fa225231f286f97113500f381141d43f6f4ae
Jing Wang <wjing.ca@gmail.com>
2018-03-07 13:20:35 -0500

    * presentation

--- src/IPD12/ProjectManagement/ProjectTasksTimeSeriesReport.java
+++ src/IPD12/ProjectManagement/ProjectTasksTimeSeriesReport.java
@@ -18,52 +18,56 @@ import net.sf.dynamicreports.report.constant.TimePeriod;
 import net.sf.dynamicreports.report.datasource.DRDataSource;
 import net.sf.dynamicreports.report.exception.DRException;
 import net.sf.jasperreports.engine.JRDataSource;
+
 public class ProjectTasksTimeSeriesReport {
+
     private String Title = "";
+
     public ProjectTasksTimeSeriesReport(String title) {
         this.Title = title;
         build();
     }
+
     private void build() {
-		FontBuilder boldFont = stl.fontArialBold().setFontSize(12);
+        FontBuilder boldFont = stl.fontArialBold().setFontSize(12);
 
-		//TextColumnBuilder<Date> orderDateColumn = col.column("Date", "Date", type.dateYearToMonthType());
-                TextColumnBuilder<Date> orderDateColumn = col.column("Date", "Date", type.dateType());
-		TextColumnBuilder<Integer> quantityColumn = col.column("AllTasks", "AllTasks", type.integerType());
-		TextColumnBuilder<Integer> priceColumn = col.column("LeftTasks", "LeftTasks", type.integerType());
+        //TextColumnBuilder<Date> orderDateColumn = col.column("Date", "Date", type.dateYearToMonthType());
+        TextColumnBuilder<Date> orderDateColumn = col.column("Date", "Date", type.dateType());
+        TextColumnBuilder<Integer> quantityColumn = col.column("AllTasks", "AllTasks", type.integerType());
+        TextColumnBuilder<Integer> priceColumn = col.column("LeftTasks", "LeftTasks", type.integerType());
 
-		try {
-			report()
-					.setTemplate(Templates.reportTemplate)
-					.columns(orderDateColumn, quantityColumn, priceColumn)
-					.title(Templates.createTitleComponent(Title))
-					.summary(
-							cht.timeSeriesChart()
-									.setTitle(Title)
-									.setTitleFont(boldFont)
-									.setTimePeriod(orderDateColumn)
-									.setTimePeriodType(TimePeriod.DAY)
-									.series(
-											cht.serie(quantityColumn), cht.serie(priceColumn))
-									.setTimeAxisFormat(
-											cht.axisFormat().setLabel("Date")))
-					.pageFooter(Templates.footerComponent)
-					.setDataSource(createDataSource())
-					.show(false);
-		} catch (DRException e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            report()
+                    .setTemplate(Templates.reportTemplate)
+                    .columns(orderDateColumn, quantityColumn, priceColumn)
+                    .title(Templates.createTitleComponent(Title))
+                    .summary(
+                            cht.timeSeriesChart()
+                                    .setTitle(Title)
+                                    .setTitleFont(boldFont)
+                                    .setTimePeriod(orderDateColumn)
+                                    .setTimePeriodType(TimePeriod.DAY)
+                                    .series(
+                                            cht.serie(quantityColumn), cht.serie(priceColumn))
+                                    .setTimeAxisFormat(
+                                            cht.axisFormat().setLabel("Date")))
+                    .pageFooter(Templates.footerComponent)
+                    .setDataSource(createDataSource())
+                    .show(false);
+        } catch (DRException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private JRDataSource createDataSource() {
-		DRDataSource dataSource = new DRDataSource("Date", "AllTasks", "LeftTasks");
-                
-                Calendar c = Calendar.getInstance();
-		c.add(Calendar.DAY_OF_MONTH, -14);
-		for (int i = 0; i < 15; i++) {
-			dataSource.add(c.getTime(),(int)(Math.random()*3)+7,(int)(Math.random()*5)+2);
-			c.add(Calendar.DAY_OF_MONTH, 1);
-		}	
-		return dataSource;
-	} 
+    private JRDataSource createDataSource() {
+        DRDataSource dataSource = new DRDataSource("Date", "AllTasks", "LeftTasks");
+
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.DAY_OF_MONTH, -14);
+        for (int i = 0; i < 15; i++) {
+            dataSource.add(c.getTime(), (int) (Math.random() * 3) + 7, (int) (Math.random() * 5) + 2);
+            c.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        return dataSource;
+    }
 }
